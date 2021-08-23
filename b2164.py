51761a816f1b0905a04c0abfa4aa1993cdfd0885
ghkdlxld <ohsg97@gmail.com>
2021-08-23 22:47:27 +0900

    BOJ

---
+++ b2164.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+
+N = int(6)
+queue = deque()
+for n in range(1, N+1):
+    queue.append(n)
+
+for _ in range(N-1):
+    queue.popleft()
+    left = queue.popleft()
+    queue.append(left)
+
+print(*queue)

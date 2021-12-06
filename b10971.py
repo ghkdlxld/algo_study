51b70dca00d462443c1334149a198f5c6d656cf5
ghkdlxld <ohsg97@gmail.com>
2021-12-07 00:37:40 +0900

    BOJ 외판원 순회2

--- b10971.py
+++ b10971.py
@@ -1,31 +1,39 @@
 import sys
 sys.stdin = open('input.txt')
 
-def trip(x, cost):
-    visited.append(x)
-    if len(visited) == N:
-        cost += W[x][visited[0]]
-        visited.clear()
-        return trip_min.append(cost)
-    else:
-        next = []
-        for j in range(1, N+1):
-            if (W[x][j] != 0) and (j not in visited):
-                next.append(j)
 
-        while next:
-            b = next.pop(0)
-            trip(b, cost + W[x][b])
-            return -1
+def trip(start, x):
+    global result, cost
+    if visited.count(0) == 0:
+        # 돌아가는 길이 있는지 없는지 확인!!!
+        if W[x][start] != 0:
+            if result > cost + W[x][start]:
+                result = cost + W[x][start]
+        return
+
+    if cost > result:
+        return
+
+    else:
+        for i in range(N):
+            # 방문하지 않았고, 연결되어 있으면
+            if visited[i] == 0 and W[x][i] != 0:
+                visited[i] = 1
+                cost += W[x][i]
+                trip(start, i)  # i로 이동
+
+                visited[i] = 0
+                cost -= W[x][i]
 
 
 N = int(input())
-W = [[0] + list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-W.insert(0, [0]*(N+1))
-visited = []
-trip_min = []
-for i in range(1, N+1):
-    trip_min.append(trip(i, 0))
-for _ in range(trip_min.count(-1)):
-    trip_min.remove(-1)
-print(min(trip_min))
+W = [list(map(int, input().split())) for _ in range(N)]
+result = 987654321
+
+for n in range(N):
+    visited = [0] * N
+    visited[n] = 2
+    cost = 0
+    trip(n, n)
+
+print(result)

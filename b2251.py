2542f0b2b7031d47051495f8a3f7a33ba36e0bea
ghkdlxld <ohsg97@gmail.com>
2021-12-19 23:31:28 +0900

    BOJ 물통

--- b2251.py
+++ b2251.py
@@ -1,15 +1,46 @@
 import sys
+from collections import deque
 sys.stdin = open('input.txt')
 
+def pour(x, y):
+    if visited[x][y] == 0:
+        visited[x][y] = 1
+        q.append([x, y])
+
 def bfs():
-    pass
+    q.append([0, 0])
+    visited[0][0] = 1
 
+    while q:
+        a, b = q.popleft()
+        c = C - (a+b)
+        if a == 0:
+            ans.append(c)
 
+        # x->y, x->z, y->x, y->z, z->x, z->y
+        # 부을 때 A, B 물통 상태
+        water = min(a, B - b)
+        pour(a-water, b+water)
 
+        water = min(a, C - c)
+        pour(a-water, b)
+
+        water = min(b, A - a)
+        pour(a + water, b - water)
+
+        water = min(b, C-c)
+        pour(a, b-water)
+
+        water = min(c, A-a)
+        pour(a+water, b)
+
+        water = min(c, B-b)
+        pour(a, b+water)
 
 A, B, C = map(int, input().split())
-bottles = [A, B, C]
+visited = [[0]*(B+1) for _ in range(A+1)]
+q = deque()
 ans = []
-visited = [[0]*3 for _ in range(3)]
 bfs()
-print(sorted(ans))
+
+print(' '.join(map(str, sorted(ans))))

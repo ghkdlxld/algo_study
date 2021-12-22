47864e75329617cdb66666f9a392f27ff5eec1ea
ghkdlxld <ohsg97@gmail.com>
2021-12-22 23:48:14 +0900

    BOJ 맥주 마시면서 걸어가기

--- b9205.py
+++ b9205.py
@@ -5,32 +5,33 @@ from collections import deque
 
 def bfs(i, j):
     global beer, walk, state
-    q = deque((i, j))
+    q = deque()
+    q.append((i, j))
     visited[i][j] = 1
 
     while q:
-        if walk == 50:
-            walk = 0
-            beer -= 1
-
-        di = [0, 0, 1, -1]
-        dj = [1, -1, 0, 0]
-        now_i, now_j = q.popleft()
-        walk += 1
-
-        if [now_i, now_j] == rock:
-            if beer > 0:
-                state = 'happy'
-
-        if visited[now_i][now_j] == 2:
-            beer = 20
-
-        for k in range(4):
-            ni = now_i + di[k]
-            nj = now_j + dj[k]
-            if 0<= ni< 32768 and 0<= nj< 32768 and visited[ni][nj] == 0:
-                q.append((ni, nj))
-                visited[ni][nj] = 1
+        # if walk == 50:
+        #     walk = 0
+        #     beer -= 1
+        #
+        # di = [0, 0, 1, -1]
+        # dj = [1, -1, 0, 0]
+        # now_i, now_j = q.popleft()
+        # walk += 1
+        #
+        # if [now_i, now_j] == rock:
+        #     if beer > 0:
+        #         state = 'happy'
+        #
+        # if visited[now_i][now_j] == 2:
+        #     beer = 20
+        #
+        # for k in range(4):
+        #     ni = now_i + di[k]
+        #     nj = now_j + dj[k]
+        #     if 0<= ni< 32768 and 0<= nj< 32768 and visited[ni][nj] == 0:
+        #         q.append((ni, nj))
+        #         visited[ni][nj] = 1
 
 
 

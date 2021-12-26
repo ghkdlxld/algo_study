ec15853fc4991cc6f0a13b66985f826f39415fbc
ghkdlxld <ohsg97@gmail.com>
2021-12-26 20:33:41 +0900

    BOJ 맥주 마시면서 걸어가기

--- b9205.py
+++ b9205.py
@@ -2,54 +2,37 @@ import sys
 sys.stdin = open('input.txt')
 from collections import deque
 
-
 def bfs(i, j):
-    global beer, walk, state
+    global state, n
     q = deque()
     q.append((i, j))
-    visited[i][j] = 1
 
     while q:
-        # if walk == 50:
-        #     walk = 0
-        #     beer -= 1
-        #
-        # di = [0, 0, 1, -1]
-        # dj = [1, -1, 0, 0]
-        # now_i, now_j = q.popleft()
-        # walk += 1
-        #
-        # if [now_i, now_j] == rock:
-        #     if beer > 0:
-        #         state = 'happy'
-        #
-        # if visited[now_i][now_j] == 2:
-        #     beer = 20
-        #
-        # for k in range(4):
-        #     ni = now_i + di[k]
-        #     nj = now_j + dj[k]
-        #     if 0<= ni< 32768 and 0<= nj< 32768 and visited[ni][nj] == 0:
-        #         q.append((ni, nj))
-        #         visited[ni][nj] = 1
+        now_i, now_j = q.popleft()
+        for i in range(n):
+            if now_i != stores[i][0] or now_j != stores[i][1]:
+                if abs(stores[i][0] - now_i) + abs(stores[i][1] - now_j) <= 1000:
+                    if abs(rock[0]-stores[i][0]) + abs(rock[1]-stores[i][1]) <= 1000:
+                        state = 'happy'
+                        return
 
-
+                    # 처음에 visited 말고 not in 을 썼더니 시간초과가 뜸
+                    if visited[i] == 0:
+                        q.append(stores[i])
+                        visited[i] = 1
 
 
 T = int(input())
 for tc in range(1, T+1):
     n = int(input())
     home = list(map(int, input().split()))
-    store = [list(map(int, input().split())) for _ in range(n)]
+    stores = [list(map(int, input().split())) for _ in range(n)]
     rock = list(map(int, input().split()))
-    visited = [[0]*32768 for _ in range(32768)]
-
-    for x in range(n):
-        visited[store[x][0]][store[x][1]] = 2
+    visited = [0]*n
 
     state = 'sad'
-    beer = 20
-    walk = 0
-    bfs(home[0], home[1])
-
+    if abs(rock[0] - home[0]) + abs(rock[1] - home[1]) <= 1000:
+        state = 'happy'
+    else:
+        bfs(*home)
     print(state)

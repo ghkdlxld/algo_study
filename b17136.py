7e0dfaffd7c81affea4d523a3617a0c480855b5b
ghkdlxld <ohsg97@gmail.com>
2021-12-29 23:50:17 +0900

    BOJ 색종이 붙이기

--- b17136.py
+++ b17136.py
@@ -1,9 +1,13 @@
 import sys
 sys.stdin = open('input.txt')
 
+def dfs(i, j):
+    pass
 
 
 
 board = [list(map(int, input().split())) for _ in range(10)]
 paper = [1, 2, 3, 4, 5]
 paper_cnt = [0]*6
+
+print(board)

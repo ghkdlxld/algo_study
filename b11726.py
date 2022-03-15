1c2da42f4def6cadf962fc663c05d355ef27adfd
ghkdlxld <ohsg97@gmail.com>
2022-03-15 23:52:04 +0900

    2*n 타일링

--- b11726.py
+++ b11726.py
@@ -1,7 +1,15 @@
 import sys
 sys.stdin = open('input.txt')
- 
 
 N = int(input())
-tile(N)
-visited = []
+cnt = 1
+t = [1]*(N // 2 + 1)
+b = [1]*(N // 2 + 1)
+ans = [1]*(N // 2 + 1)
+for i in range(1, N // 2+1):
+    for j in range(N-i,N-2*i,-1):
+        t[i] *= j
+    b[i] = b[i-1]*i
+    cnt += t[i]/b[i]
+
+print(int(cnt))

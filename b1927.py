945055839692344d93fd76c0684f836923016bb5
Sookyeong Oh <61732437+ghkdlxld@users.noreply.github.com>
2022-04-10 23:49:52 +0900

    최소 힙 풀이중
    힙 사용해서 풀기

--- b1927.py
+++ b1927.py
@@ -1,7 +1,30 @@
 import sys
-sys.stdin = open('input.txt')
-input = sys.stdin.readline
+from collections import deque
 
-N = int(input())
-        
-print(arr)
+def bfs(i):
+    q = deque()
+    q.append(i)
+    visited[i] = 1
+
+    while q:
+        now = q.popleft()
+        for x in link[now]:
+            if visited[x] == 0:
+                q.append(x)
+                visited[x] = 1
+
+N, M = map(int, sys.stdin.readline().split())
+link = [[] for _ in range(N+1)]
+for _ in range(M):
+    u, v = map(int, sys.stdin.readline().split())
+    link[u].append(v)
+    link[v].append(u)
+  
+visited = [0]*(N+1)
+cnt = 0
+for i in range(1, N+1):
+    if visited[i] == 0:
+        bfs(i)
+        cnt += 1
+
+print(cnt)

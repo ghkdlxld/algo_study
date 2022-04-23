65ac6d71a7180c7c2306a815af6805ceb241c3d6
ghkdlxld <ohsg97@gmail.com>
2022-04-24 00:11:54 +0900

    최대 힙

--- b11279.py
+++ b11279.py
@@ -1,1 +1,21 @@
-# 최대힙
+import sys
+import heapq
+sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
+N = int(input())
+heap = []
+for _ in range(N):
+    x = int(input())
+    if x == 0:
+        if len(heap) == 0:
+            print(0)
+        else:
+            # 가장 큰 값 출력, pop
+            print(-heap[0])
+            heapq.heappop(heap)
+
+    else:
+        heapq.heappush(heap, -x)
+
+

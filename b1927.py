e43cedc4f682f0da4ccf3f6b30f2f63c287ba621
ghkdlxld <ohsg97@gmail.com>
2022-04-23 22:48:33 +0900

    최소 힙

--- b1927.py
+++ b1927.py
@@ -1,33 +1,18 @@
-# 시간초과 코드
-
 import sys
-sys.stdin = open('input.txt')
-from collections import deque
+import heapq
 input = sys.stdin.readline
 
 N = int(input())
-arr = deque()
+heap = []
 for _ in range(N):
     x = int(input())
 
     if x == 0:
-        if len(arr) == 0:
+        if len(heap) == 0:
             print(0)
         else:
-            print(arr[0])
-            arr.popleft()
-
+            print(heap[0])
+            heapq.heappop(heap)
 
     else:
-        arr.append(x)
-        if len(arr) == 1:
-            continue
-
-        else:
-            i = len(arr)-1
-            while True:
-                if i != 0 and arr[i] < arr[i-1]:
-                    arr[i], arr[i - 1] = arr[i-1], arr[i]
-                    i -= 1
-                else:
-                    break
+        heapq.heappush(heap, x)

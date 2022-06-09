6d9c9f22af72d409405e301992dc0ee60216dd4a
ghkdlxld <ohsg97@gmail.com>
2022-06-09 21:44:17 +0900

    미로 탐색

--- b2178.py
+++ b2178.py
@@ -1,6 +1,24 @@
 import sys
 sys.stdin = open('input.txt')
+from collections import deque
 
-N, M = map(int, input().split())
+def bfs():
+    q = deque()
+    q.append((0, 0))
+    di = [0, 0, 1, -1]
+    dj = [1, -1, 0, 0]
+
+    while q:
+        now = q.popleft()
+        visited[now[0], now[1]] = 1
+        for k in range(4):
+            ni = now + di[k]
+            nj = now + dj[k]
+
+
+
+N, M = map(int, input().split()) # 1 ~ N , 1 ~ M
 miro = [list(map(int, list(input()))) for _ in range(N)]
-print(miro)
+visited = [[0]*M for _ in range(N)]
+
+bfs()

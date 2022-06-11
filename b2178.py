f3e8b946e816890aa3f1205be1ba6f5df0e056b1
ghkdlxld <ohsg97@gmail.com>
2022-06-11 23:06:45 +0900

    BOJ 미로탐색

--- b2178.py
+++ b2178.py
@@ -1,24 +1,29 @@
 import sys
 sys.stdin = open('input.txt')
 from collections import deque
+input = sys.stdin.readline
 
 def bfs():
     q = deque()
     q.append((0, 0))
+    visited[0][0] = 1
     di = [0, 0, 1, -1]
     dj = [1, -1, 0, 0]
 
     while q:
         now = q.popleft()
-        visited[now[0], now[1]] = 1
         for k in range(4):
-            ni = now + di[k]
-            nj = now + dj[k]
+            ni = now[0] + di[k]
+            nj = now[1] + dj[k]
 
+            if 0 <= ni < N and 0 <= nj < M and miro[ni][nj] == 1 and visited[ni][nj] == 0:
+                q.append((ni, nj))
+                visited[ni][nj] = visited[now[0]][now[1]] + 1
+                if ni == N - 1 and nj == M - 1:
+                    return visited[ni][nj]
 
-
-N, M = map(int, input().split()) # 1 ~ N , 1 ~ M
-miro = [list(map(int, list(input()))) for _ in range(N)]
+N, M = map(int, input().split())
+miro = [list(map(int, list(input().strip()))) for _ in range(N)]
 visited = [[0]*M for _ in range(N)]
-
-bfs()
+ans = bfs()
+print(ans)

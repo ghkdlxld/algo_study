521b19b4b58036413192e874e5430a508ee3d050
ghkdlxld <ohsg97@gmail.com>
2022-06-12 01:25:01 +0900

    BOJ 이중 우선순위 큐

--- b7662.py
+++ b7662.py
@@ -1,33 +1,43 @@
 import sys
+sys.stdin = open('input.txt')
 input = sys.stdin.readline
 import heapq
 
 T = int(input())
-q = []
-
 for tc in range(T):
+    max_q = []
+    min_q = []
+    key = [False]*1000001
     k = int(input())
-    for _ in range(k):
+    for i in range(k):
         method, n = input().split()
         if method == 'I':
-            heapq.heappush(q, int(n))
+            heapq.heappush(min_q, (int(n), i))
+            heapq.heappush(max_q, (-int(n), i))
+            key[i] = True
 
         else:
-            if len(q) == 0:
-                continue
-
-            elif int(n) < 0:
-                heapq.heappop(q)
+            if int(n) < 0:
+                if len(min_q) == 0:
+                    continue
+                x = heapq.heappop(min_q)
+                key[x[1]] = False
 
             else:
-                q.sort()
-                q.pop(-1)
+                if len(max_q) == 0:
+                    continue
+                x = heapq.heappop(max_q)
+                key[x[1]] = False
+
+            while max_q and not key[max_q[0][1]]:
+                heapq.heappop(max_q)
+            while min_q and not key[min_q[0][1]]:
+                heapq.heappop(min_q)
 
 
 
-
-    if len(q) == 0:
+    if len(max_q) == 0:
         print('EMPTY')
 
     else:
-        print(q[-1], q[0])
+        print(-max_q[0][0], min_q[0][0])

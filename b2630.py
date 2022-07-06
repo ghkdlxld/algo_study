3424fdc92763f5b2c96c350fb037687ab1749de7
ghkdlxld <ohsg97@gmail.com>
2022-07-06 23:12:26 +0900

    색종이 만들기

--- b2630.py
+++ b2630.py
@@ -1,12 +1,12 @@
 import sys
 sys.stdin = open('input.txt')
 
+def make_paper():
+    pass
+
+
 N = int(input())
 paper = [list(map(int, input().split())) for _ in range(N)]
 
    
-
-   
-
-
-print(paper)
+make_paper()

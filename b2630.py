050f5c8d25c2f97049f86c386a1b7f796d788d6b
ghkdlxld <ohsg97@gmail.com>
2022-07-28 22:31:31 +0900

    색종이 만들기

--- b2630.py
+++ b2630.py
@@ -1,5 +1,6 @@
 import sys
 sys.stdin = open('input.txt')
+from collections import deque
 
 # 시작점 i,j 변의 길이 m, 색깔
 # True 모두 같음
@@ -12,7 +13,26 @@ def same(i, j, m, k):
 
 N = int(input())
 paper = [list(map(int, input().split())) for _ in range(N)]
-print(paper)
-stack = [(0, 0, N)]
+visited = [[0]*N for _ in range(N)]
+ans = [0, 0]
+q = deque()
+q.append((0, 0))
+
+m = N
+while q:
+    now = q.popleft()
+    if same(*now, m, paper[now[0]][now[1]]):
+        ans[paper[now[0]][now[1]]] += 1
+        for a in range(m):
+            for b in range(m):
+                visited[now[0]+a][now[1]+b] = 1
+    else:
+        if q:
+
+        m *= 2
+        for a in range(2):
+            for b in range(2):
+                q.append((now[0]+(N//m+1)*a, now[1]+(N//m + 1)*b))
 
 
+print(ans)

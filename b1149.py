f050c16ce938bf16d7288c3ab6b35ce4f0b4d2ae
ghkdlxld <ohsg97@gmail.com>
2022-08-26 23:42:37 +0900

    RGB거리

--- b1149.py
+++ b1149.py
@@ -6,13 +6,16 @@ N = int(input())
 cost = [list(map(int, input().split())) for _ in range(N)]
 
 ans = int(1e9)
-dp = [[]*3 for _ in range(N)]
-
-print(dp)
+dp = [[0]*3 for _ in range(N)]
+ans = 0
 
 def home(x):
     for k in range(3):
-        if x
+
+
+
+
 
 
 home(1)
+

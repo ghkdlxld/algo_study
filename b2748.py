903b9d8bfad2b1b4e28fb90225e49e5680ce811a
ghkdlxld <ohsg97@gmail.com>
2022-09-11 23:51:37 +0900

    피보나치수2

---
+++ b2748.py
@@ -0,0 +1,16 @@
+import sys
+sys.stdin = open('input.txt')
+
+
+def fibo(x):
+    if x == 0:
+        return 0
+
+    elif x == 1:
+        return 1
+
+    else:
+        return fibo(x-1) + fibo(x-2)
+
+n = int(input())
+print(fibo(n))

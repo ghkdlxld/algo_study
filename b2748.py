58037a57ac7cb2c8a50b17a77d672ccc1d7ed7e5
ghkdlxld <ohsg97@gmail.com>
2022-09-12 00:02:30 +0900

    BOJ 피보나치 수2

--- b2748.py
+++ b2748.py
@@ -3,14 +3,17 @@ sys.stdin = open('input.txt')
 
 
 def fibo(x):
-    if x == 0:
-        return 0
-
-    elif x == 1:
-        return 1
+    if dp[x]:
+        return dp[x]
 
     else:
-        return fibo(x-1) + fibo(x-2)
+        dp[x] = fibo(x-1) + fibo(x-2)
+        return dp[x]
 
 n = int(input())
+arr = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597]
+dp = [False]*91
+for a in range(len(arr)):
+    dp[a] = arr[a]
+
 print(fibo(n))

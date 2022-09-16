a6448d264761b2a453bff75f70817a688dbe5046
ghkdlxld <ohsg97@gmail.com>
2022-09-17 01:13:17 +0900

    BOJ 퇴사2

--- b15486.py
+++ b15486.py
@@ -3,5 +3,11 @@ sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
 N = int(input())
-table = [list(map(int, input().split())) for _ in range(N)]
-print(table)
+table = [[0, 0]] + [list(map(int, input().split())) for _ in range(N)]
+dp = [0]*(N+1)
+for i in range(1, N+1):
+    if i+table[i][0]-1 <= N:
+        dp[i+table[i][0]-1] = max(dp[i-1]+table[i][1], dp[i+table[i][0]-1])
+    dp[i] = max(dp[i-1], dp[i])
+
+print(dp[-1])

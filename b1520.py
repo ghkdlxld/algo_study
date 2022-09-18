1562967d16c2ebbbb9c344bf2b8ca3d374225e89
ghkdlxld <ohsg97@gmail.com>
2022-09-18 23:53:10 +0900

    dfs 메모이제이션

--- b1520.py
+++ b1520.py
@@ -2,6 +2,20 @@ import sys
 sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
+def dfs(i, j):
+    stack = []
+    stack.append([i,j])
+
+    di = [0,0,1,-1]
+    dj = [1, -1, 0, 0]
+
+
+
+
+
+
 M, N = map(int, input().split())
 A = [list(map(int, input().split())) for _ in range(M)]
 print(A)
+
+dfs(0,0)

03d99b8ed5b3e2ca74450ea417e746cc313d6063
ghkdlxld <ohsg97@gmail.com>
2022-09-19 23:33:36 +0900

    dfs 시간초과

--- b1520.py
+++ b1520.py
@@ -1,21 +1,27 @@
 import sys
-sys.stdin = open('input.txt')
+sys.setrecursionlimit(100000)
 input = sys.stdin.readline
 
 def dfs(i, j):
-    stack = []
-    stack.append([i,j])
 
-    di = [0,0,1,-1]
+    if i == M-1 and j == N-1:
+        visited[i][j] += 1
+        return
+
+    di = [0, 0, 1, -1]
     dj = [1, -1, 0, 0]
 
+    for k in range(4):
+        ni = i + di[k]
+        nj = j + dj[k]
 
-
+        if 0 <= ni < M and 0 <= nj < N and A[ni][nj] < A[i][j]:
+            dfs(ni, nj)
 
 
 
 M, N = map(int, input().split())
 A = [list(map(int, input().split())) for _ in range(M)]
-print(A)
-
+visited = [[0]*N for _ in range(M)]
 dfs(0,0)
+print(visited[-1][-1])

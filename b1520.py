7b5431c6404d867bcb216e685581793e4c0b766a
Sookyeong Oh <ohsg97@gmail.com>
2022-09-20 23:56:36 +0900

    Update b1520.py

--- b1520.py
+++ b1520.py
@@ -3,11 +3,11 @@ sys.setrecursionlimit(100000)
 input = sys.stdin.readline
 
 def dfs(i, j):
-
     if i == M-1 and j == N-1:
-        visited[i][j] += 1
-        return
-
+        return 1
+    
+    visited[i][j] = 1
+    
     di = [0, 0, 1, -1]
     dj = [1, -1, 0, 0]
 
@@ -16,12 +16,12 @@ def dfs(i, j):
         nj = j + dj[k]
 
         if 0 <= ni < M and 0 <= nj < N and A[ni][nj] < A[i][j]:
-            dfs(ni, nj)
+            visited[i][j] = dfs(ni, nj)
 
 
 
 M, N = map(int, input().split())
 A = [list(map(int, input().split())) for _ in range(M)]
-visited = [[0]*N for _ in range(M)]
+visited = [[-1]*N for _ in range(M)]
 dfs(0,0)
 print(visited[-1][-1])

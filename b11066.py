3760c925c7bc84fc5ea45c00b6b2ef9e29b81596
ghkdlxld <ohsg97@gmail.com>
2022-09-23 13:56:00 +0900

    dp 반씩 합 쪼개서비교: 합이 아님

--- b11066.py
+++ b11066.py
@@ -6,5 +6,17 @@ T = int(input())
 for tc in range(1, T+1):
     K = int(input())
     paper = list(map(int, input().split()))
-    dp = [0]*K
+    d = []
+
+    def dp(l, r):
+        tmp = [0, 1e9]
+        for i in range(r):
+            if sum(paper[l:i]) + sum(paper[i:r]) < tmp[1]:
+                tmp = [i, sum(paper[l:i]) + sum(paper[i:r])]
+        d.append(tmp[1])
+        dp(0, tmp[0])
+        dp(tmp[0], r)
+
+
+    dp(0, K)
 

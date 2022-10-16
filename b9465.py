601082b17e04753c3c14cfef957420f0d8f8554b
ghkdlxld <ohsg97@gmail.com>
2022-10-16 23:54:10 +0900

    스티커

--- b9465.py
+++ b9465.py
@@ -5,4 +5,14 @@ T = int(input())
 for tc in range(1, T+1):
     n = int(input())
     sticker = [list(map(int, input().split())) for _ in range(2)]
+
+
+    dp = [[0]*n for _ in range(2)]
+    dp[0][0] = sticker[0][0]
+    dp[1][0] = sticker[1][0]
+
     print(sticker)
+    print(dp)
+    for i in range(2):
+        for j in range(1, n):
+            dp[i][j] = max()

d624b5c3a94522e93cf8e4ca5ea41be1d3049786
ghkdlxld <ohsg97@gmail.com>
2022-10-20 17:16:14 +0900

    BOJ 연산자 끼워넣기

--- b14888.py
+++ b14888.py
@@ -1,23 +1,36 @@
 import sys
 sys.stdin = open('input.txt')
+input = sys.stdin.readline
 
 N = int(input())
 num = list(map(int, input().split()))
 cal = list(map(int, input().split())) # +,-,*,//
 visited = [0]*4
-ans = 0
+ans_max = -int(1e9)
+ans_min = int(1e9)
 
 def sol(x, tmp):
-    if x == N and tmp > ans:
+    global ans_max, ans_min
+
+    if x == N:
+        ans_max = max(ans_max, tmp)
+        ans_min = min(ans_min, tmp)
         return
 
-    for i in range(4):
-        if cal[i]-visited[i] > 0:
-            visited[i] += 1
-            sol()
+    for k in range(4):
+        if cal[k] > visited[k]:
+            visited[k] += 1
+            if k == 0:
+                sol(x + 1, tmp+num[x])
+            elif k == 1:
+                sol(x+1, tmp-num[x])
+            elif k == 2:
+                sol(x+1, tmp*num[x])
+            else:
+                sol(x+1, int(tmp/num[x]))
 
+            visited[k] -= 1
 
-
-
-
-sol(0, num[0])
+sol(1, num[0])
+print(ans_max)
+print(ans_min)

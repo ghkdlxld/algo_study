06e1baceeb72a3662068e6d8c16d4b3079f5aa54
sookyeong.oh <ohsg97@gmail.com>
2022-12-05 16:29:16 +0900

    합치고 나누기 함수 작성중

--- b20056.py
+++ b20056.py
@@ -4,4 +4,41 @@ input = sys.stdin.readline
 
 N, M, K = map(int, input().split())
 fireball = [list(map(int, input().split())) for _ in range(M)]
-print(fireball)
+arr = [[[] for _ in range(N)] for _ in range(N)]
+direction = [[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1]]
+curl = 0
+
+for r, c, m, s, d in fireball:
+    arr[r-1][c-1].append([m, s, d])
+
+for a in arr:
+    print(a, end='\n')
+
+print('------------------------')
+
+# 이동
+def fire(new_arr):
+    for i in range(N):
+        for j in range(N):
+            if len(arr[i][j]) > 0:
+                for m, s, d in arr[i][j]:
+                    r, c = direction[d]
+                    new_arr[(i+r*s)%N][(j+c*s)%N].append([m, s, d])
+
+    return new_arr
+
+
+# 합치고 나누기
+def divide():
+    for a in arr:
+        print(a, end='\n')
+
+
+
+
+
+if curl < K:
+    curl += 1
+    new_arr = [[[] for _ in range(N)] for _ in range(N)]
+    arr = fire(new_arr)
+    divide()

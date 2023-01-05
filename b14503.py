29389d753c59ab8076da9040731c9134ad1eff36
sookyeong.oh <ohsg97@gmail.com>
2023-01-05 14:43:31 +0900

    회전 재귀 수정필요

--- b14503.py
+++ b14503.py
@@ -4,22 +4,43 @@ input = sys.stdin.readline
 
 N, M = map(int, input().split())
 r, c, d = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(N)]
+visited = [list(map(int, input().split())) for _ in range(N)]
 dir = {0: [-1,0], 1:[0,-1], 2:[1,0], 3:[0,1]}
 cnt = 0
 
-print(arr)
 
 def clean(i, j, d):
+    global cnt
     # 현 위치 청소 -> 방문하지 않은 경우만 cnt += 1
+    if visited[i][j] != 1:
+        visited[i][j] = 1
+        cnt += 1
 
     # d 기준 왼쪽부터 탐색 for
+    for k in range(4):
+        new_dir = (d + 1) % 4
+        new_i = i+dir[new_dir][0]
+        new_j = j+dir[new_dir][1]
+        # 청소 가능 -> (d+1)%4로 회전, 전진 재귀, break
+        if 0 <= new_i <N and 0 <= new_j <M and visited[new_i][new_j] == 0:
+            clean(new_i, new_j, new_dir)
+            break
 
-    # 청소 가능 -> (d+1)%4로 회전, 전진 재귀, break
-    # 청소 불가능 -> (d+1)%4로 회전 재귀, break
+        # 청소 불가능(벽/이미청소) -> (d+1)%4로 회전 재귀, break
+        else:
+            d = new_dir
 
     # for를 다 돌아서 d 로 제자리로 온 경우
     # 후진 가능 -> (d+2)%4로 전진(후진) d 그대로 재귀
+    back_i = i + dir[(d+2)%4][0]
+    back_j = j + dir[(d+2)%4][1]
+    if 0 <= back_i <N and 0 <= back_j <M and visited[back_i][back_j] == 0:
+        clean(back_i, back_j, d)
+
     # 후진 불가능 -> return
+    else:
+        return
 
 
+clean(r, c, d)
+print(cnt)

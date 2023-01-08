15f534a7a5d7ba798a3309c35ad80a309bd37adc
ghkdlxld <ohsg97@gmail.com>
2023-01-08 23:04:30 +0900

    좋아하는 학생리스트 생성

--- b21608.py
+++ b21608.py
@@ -3,6 +3,9 @@ sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
 N = int(input())
-seat = [ list(map(int, input().split())) for _ in range(N**2)]
+students = [[] for _ in range(N**2+1)]
+for x in range(N**2):
+    tmp = list(map(int, input().split()))
+    students[tmp[0]] = tmp[1:]
 
-print(seat)
+print(students)

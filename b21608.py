8126f9b4567871d016a171780e11ea1739574ee4
ghkdlxld <ohsg97@gmail.com>
2023-01-11 23:36:22 +0900

    BOJ 상어 초등학교

--- b21608.py
+++ b21608.py
@@ -14,30 +14,30 @@ di = [0, 0, 1, -1]
 dj = [1, -1, 0, 0]
 ans = 0
 
-def place_std(x, y):
+def place_std(lst):
     for i in range(N):
         for j in range(N):
-            tmp = [i, j, 0, 0] # 현 위치 i, j / like, empty
-            for k in range(4):
-                ni = i + di[k]
-                nj = j + dj[k]
+            std = [i, j, 0, 0] # 현 위치 i, j / like, empty
+            if seat[i][j] == 0:
+                for k in range(4):
+                    ni = i + di[k]
+                    nj = j + dj[k]
 
-                if seat[i][j] == 0 and 0 <= ni < N and 0 <= nj < N:
-                    if seat[ni][nj] == 0:
-                        tmp[3] += 1
-                    elif seat[ni][nj] in y:
-                        tmp[2] += 1
-            like_lst.append(tmp)
+                    if 0 <= ni < N and 0 <= nj < N:
+                        if seat[ni][nj] == 0:
+                            std[3] += 1
+                        if seat[ni][nj] in lst:
+                            std[2] += 1
+                like_lst.append(std)
 
 
 
 for k, v in students.items():
     like_lst = []
-    place_std(k, v)
-    like_lst = sorted(like_lst, key=lambda x:[-x[2], -x[3]])
+    place_std(v)
+    like_lst = sorted(like_lst, key=lambda x: [-x[2], -x[3], x[0], x[1]])
     check_seat = like_lst[0]
     seat[check_seat[0]][check_seat[1]] = k
-
 
 
 for a in range(N):
@@ -47,8 +47,9 @@ for a in range(N):
             na = a + di[k]
             nb = b + dj[k]
 
-            if 0 <= na < N and 0 <= nb < N and (seat[na][nb] in students[seat[a][b]]):
-                cnt += 1
+            if 0 <= na < N and 0 <= nb < N:
+                if seat[na][nb] in students[seat[a][b]]:
+                    cnt += 1
 
         ans += int(10 ** (cnt - 1))
 

033bef8d3b3d78f90957993b4f3fb9343ce0d581
ghkdlxld <ohsg97@gmail.com>
2023-01-13 22:32:06 +0900

    아기상어 입력

--- b16236.py
+++ b16236.py
@@ -1,19 +1,8 @@
 import sys
 sys.stdin = open('input.txt')
 input = sys.stdin.readline
-from collections import deque
 
 N = int(input())
-space = [list(map(int, input().split())) for _ in range(N)]
+sea = [list(map(int, input().split())) for _ in range(N)]
 
-print(space)
-
-def fish(i, j, cnt):
-    for k in range()
-
-
-
-for i in range(N):
-    for j in range(N):
-        if space[i][j] == 9:
-            fish(i, j, 0)
+print(sea)

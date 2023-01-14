0b19ef0aab3122b6da7b4216da66ee0276ffb7cb
ghkdlxld <ohsg97@gmail.com>
2023-01-14 23:46:43 +0900

    먹을 수있는 물고기 찾기

--- b16236.py
+++ b16236.py
@@ -4,5 +4,19 @@ input = sys.stdin.readline
 
 N = int(input())
 sea = [list(map(int, input().split())) for _ in range(N)]
+baby = [0, 0]
+
 
 print(sea)
+
+# 초기
+for x in range(N):
+    for y in range(N):
+        if sea[x][y] == 9:
+            baby = [x, y]
+
+print(baby)
+
+# 먹을 수 있는 물고기 찾기
+def can_eat():
+    pass

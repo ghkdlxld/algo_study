f48e4b4868783d6f0a8ecd2bc4efde736b45965b
ghkdlxld <ohsg97@gmail.com>
2023-01-30 23:53:09 +0900

    이동 가능여부 검토함수 작성

--- b16236.py
+++ b16236.py
@@ -5,7 +5,7 @@ input = sys.stdin.readline
 N = int(input())
 sea = [list(map(int, input().split())) for _ in range(N)]
 baby = [0, 0, 2]
-
+time = 0
 
 print(sea)
 
@@ -26,10 +26,9 @@ def eat():
                 can_eat.append([i, j, abs(i-baby[0])+abs(j-baby[1])]) # i,j, 거리
 
 
-# 먹으러 갈 수 있는지
+# r,c 에서 시작해서 baby[0], baby[1]까지 이동 가능여부, visited 필요
 def can_go(r, c):
-    # r,c 에서 시작해서 baby[0], baby[1]까지 이동 가능여부
-    # 이동 가능 -> 이동, can_eat 다시 점검?
+    # 이동 가능 -> 이동
     # 이동 불가능
     pass
 
@@ -38,18 +37,19 @@ def can_go(r, c):
 
 while True:
     eat()
+    # 더이상 먹을 수 있는 고기가 없으면
     if len(can_eat) == 0:
         break
 
-    # 먹을 수 있는 물고기 중 거리가 가까운 순으로 정렬
+    # 먹을 수 있는 물고기 중 거리가 가까운 순
     can_eat = sorted(can_eat, key=lambda x : [x[2], x[0], x[1]])
 
     for a, b, c in can_eat:
-        can_go(a, b)
+        # 먹으러 갈 수 있으면
+        if can_go(a, b):
+            # 그 위치로 이동, 물고기 먹기, can_eat 초기화
+            baby = [a, b, baby[2]+sea[a][b]]
+            can_eat = []
+            break
 
-
-    can_eat = []
-
-
-# 아니면 아예 가까운곳, i,j 작은순 부터 순회 -> 갈 수 있으면 이동 방법 2
-# 전체 점검은 너무 오래걸릴듯
+print(time)

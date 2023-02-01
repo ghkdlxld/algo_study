5917d0c2ca7ede23ad45e8bc2c0ea1aa61425fa6
ghkdlxld <ohsg97@gmail.com>
2023-02-01 23:43:59 +0900

    이동 가능여부 작성

--- b16236.py
+++ b16236.py
@@ -1,4 +1,5 @@
-import sys
+import sys, copy
+from collections import deque
 sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
@@ -13,7 +14,7 @@ print(sea)
 for x in range(N):
     for y in range(N):
         if sea[x][y] == 9:
-            baby = [x, y] # 아기상어 위치
+            baby[0], baby[1] = x, y # 아기상어 위치
 
 print(baby)
 
@@ -27,8 +28,25 @@ def eat():
 
 
 # r,c 에서 시작해서 baby[0], baby[1]까지 이동 가능여부, visited 필요
-def can_go(r, c):
+di = [0, 0, 1, -1]
+dj = [1, -1, 0, 0]
+def can_go(r, c, visited):
     # 이동 가능 -> 이동
+    q = deque([r,c])
+
+    visited[r][c] = 1
+
+    while q:
+        now_i, now_j = r, c
+
+        for k in range(4):
+            ni = now_i + di[k]
+            nj = now_j + dj[k]
+
+            if 0 <= ni < N and 0 <= nj < N and visited[ni][nj] <= baby[2]: # visited 범위 수정 필요
+                pass
+
+
     # 이동 불가능
     pass
 
@@ -46,7 +64,7 @@ while True:
 
     for a, b, c in can_eat:
         # 먹으러 갈 수 있으면
-        if can_go(a, b):
+        if can_go(a, b, copy.deepcopy(sea)):
             # 그 위치로 이동, 물고기 먹기, can_eat 초기화
             baby = [a, b, baby[2]+sea[a][b]]
             can_eat = []
